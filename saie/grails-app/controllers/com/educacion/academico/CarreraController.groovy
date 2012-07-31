39ed212e197a99402c038567997cde2008971d29
jorgeskibicki@gmail.com <jorgeskibicki@gmail.com@0d437cea-b1ad-c05f-6bf1-ed9c53d5eec9>
2012-07-31 19:57:33 +0000

    New changelist

--- saie/grails-app/controllers/com/educacion/academico/CarreraController.groovy
+++ saie/grails-app/controllers/com/educacion/academico/CarreraController.groovy
@@ -758,6 +758,11 @@ class CarreraController {
 	def reportecarreras = {
 		log.info "INGRESANDO AL CLOSURE carrerasreporte"
 		log.info "PARAMETROS: $params"
+
+        params.put("nombreinstitucion", g.message(code:"caratula.institucion.nombre"))
+        params.put("direccioninstitucion", g.message(code:"caratula.institucion.direccion"))
+        params.put("telefonoinstitucion", g.message(code:"caratula.institucion.telefono"))
+
 		params.put("SUBREPORT_DIR",servletContext.getRealPath("/reports/carrera/"))
 		params.put("_format","PDF")
 		params.put("_name","reportecarreras")
@@ -776,6 +781,7 @@ class CarreraController {
 				
 			}
 		}
+
 		chain(controller:'jasper',action:'index',model:[data:listCarreras],params:params)
 
 	}

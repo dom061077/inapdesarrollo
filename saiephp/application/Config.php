44b1679fe7d3635583aaefcdacb99336f044fd11
dom061077 <dom061077@0d437cea-b1ad-c05f-6bf1-ed9c53d5eec9>
2012-10-23 20:04:12 +0000

    Import inicial

---
+++ saiephp/application/Config.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * -------------------------------------
+ * www.dlancedu.com | Jaisiel Delance
+ * framework mvc basico
+ * Config.php
+ * -------------------------------------
+ */
+
+
+define('BASE_URL', 'http://localhost/mvc/');
+define('DEFAULT_CONTROLLER', 'index');
+define('DEFAULT_LAYOUT', 'default');
+
+define('APP_NAME', 'SAIE.NET');
+define('APP_SLOGAN', 'SAIE');
+define('APP_COMPANY', 'www.inap.com.ar');
+
+define('DB_HOST', '10.0.0.199');
+define('DB_USER', 'root');
+define('DB_PASS', 'exito');
+define('DB_NAME', 'cruzroja');
+define('DB_CHAR', 'utf8');
+
+?>
